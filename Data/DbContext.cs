a22131e27126f4938cb3145fbe2792041ea6bab7
ST10355869 <128582083+ST10355869@users.noreply.github.com>
2025-05-07 13:35:00 +0200

    Add project files.

---
+++ Data/DbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using enterpriseP2.Models;
+
+
+namespace enterpriseP2.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<ProductModel> Product { get; set; }
+    }
+    
+}
---
+++ Models/ProductModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace enterpriseP2.Models
+{
+    public class ProductModel
+    {
+        [Key]
+        public int Id { get; set; }
+    public string Name { get; set; }
+        public string Category { get; set; }
+        public double Price { get; set; }
+        public DateOnly DateAdded { get; set; }
+    }
+}
