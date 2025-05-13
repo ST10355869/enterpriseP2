e708c579eecd8de0ab74d55b4b8b8083704ea76a
ST10355869 <128582083+ST10355869@users.noreply.github.com>
2025-05-13 11:28:08 +0200

    addded controller and views for employees with scaffolding

---
+++ Controllers/EmployeeModelsController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using enterpriseP2.Data;
+using enterpriseP2.Models;
+
+namespace enterpriseP2.Controllers
+{
+    public class EmployeeModelsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public EmployeeModelsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: EmployeeModels
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Employees.ToListAsync());
+        }
+
+        // GET: EmployeeModels/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employeeModel = await _context.Employees
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (employeeModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(employeeModel);
+        }
+
+        // GET: EmployeeModels/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EmployeeModels/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,Username,Password,Role")] EmployeeModel employeeModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(employeeModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employeeModel);
+        }
+
+        // GET: EmployeeModels/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employeeModel = await _context.Employees.FindAsync(id);
+            if (employeeModel == null)
+            {
+                return NotFound();
+            }
+            return View(employeeModel);
+        }
+
+        // POST: EmployeeModels/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Username,Password,Role")] EmployeeModel employeeModel)
+        {
+            if (id != employeeModel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(employeeModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EmployeeModelExists(employeeModel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employeeModel);
+        }
+
+        // GET: EmployeeModels/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employeeModel = await _context.Employees
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (employeeModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(employeeModel);
+        }
+
+        // POST: EmployeeModels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var employeeModel = await _context.Employees.FindAsync(id);
+            if (employeeModel != null)
+            {
+                _context.Employees.Remove(employeeModel);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EmployeeModelExists(int id)
+        {
+            return _context.Employees.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Services/EmployeeServices.cs
@@ -0,0 +1,24 @@
+﻿using enterpriseP2.Data;
+using enterpriseP2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace enterpriseP2.Services
+{
+    public class EmployeeServices
+    {
+        private readonly AppDbContext _context;
+        private readonly AuthenticationService _authService;
+
+        public EmployeeServices(AppDbContext context, AuthenticationService authService)
+        {
+            _context = context;
+            _authService = authService;
+        }
+        public async Task<List<ProductModel>> GetProductsByFarmer(int farmerId)
+        {
+            return await _context.Products
+                .Where(p => p.FarmerId == farmerId)
+                .ToListAsync();
+        }
+    }
+}
